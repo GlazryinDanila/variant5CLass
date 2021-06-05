7569e2858ec1dc2d1400a6fc4311beb98c789237
169 <danilglazyrin1@gmail.com>
2021-06-05 15:24:56 +0400

    te
    te

---
+++ ConsoleApp27/ConsoleApp27/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Class1 variant5 = new Class1();
+
+          
+            Console.Write("Введите число:");
+
+            Console.Write("");
+          variant5.i = int.Parse(Console.ReadLine());
+
+            switch (variant5.i)
+            {
+
+                case 12: Console.WriteLine("Зима"); break;
+                case 1: Console.WriteLine("Зима"); break;
+                case 2: Console.WriteLine("Зима"); break;
+                case 3: Console.WriteLine("Весна"); break;
+                case 4: Console.WriteLine("Весна"); break;
+                case 5: Console.WriteLine("Весна"); break;
+                case 6: Console.WriteLine("Лето"); break;
+                case 7: Console.WriteLine("Лето"); break;
+                case 8: Console.WriteLine("Лето"); break;
+                case 9: Console.WriteLine("Осень"); break;
+                case 10: Console.WriteLine("Осень"); break;
+                case 11: Console.WriteLine("Осень"); break;
+
+            }
+        }
+
+    }
+}
